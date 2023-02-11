166e673c3e4b18d35daabd5729dbe40de4e2acc2
Volodya <volodechka00@gmail.com>
2023-02-11 03:08:06 +0300

    Initial commit

---
+++ app/src/main/java/com/vladimir_tsurko/todoapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.vladimir_tsurko.todoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.vladimir_tsurko.todoapp.ui.theme.TodoAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TodoAppTheme {
+
+            }
+        }
+    }
+}
+
+
